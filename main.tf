ec1ec28538277ca5022e36b8bfcd3abd752d8524
DanielleSLRecordings <164554798+DanielleSLRecordings@users.noreply.github.com>
2024-05-07 20:04:00 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,12 @@ resource "aws_s3_bucket" "my_bucket" {
     Environment = "Dev"
   }
 }
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Output the VPC ID
+output "vpc_id" {
+  value = aws_vpc.main.id
+}

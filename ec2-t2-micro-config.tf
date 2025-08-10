af9378d1486e8ddede98562c0b034cc51b701a94
Iva Dukic Kowalska <138592614+ivadukickowalska@users.noreply.github.com>
2025-08-10 21:58:50 +0200

    Update ec2-t2-micro-config.tf
    getting rid of the t2micro

--- ec2-t2-micro-config.tf
+++ ec2-t2-micro-config.tf
@@ -28,14 +28,14 @@ data "aws_ami" "amazon_linux" {
 }
 
 # Create EC2 instance in the default subnet
-resource "aws_instance" "budget_test" {
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = "t2.micro"
-  subnet_id     = tolist(data.aws_subnets.default.ids)[0]
-  
-  tags = {
-    Name = "BudgetTestInstance"
-    Purpose = "Testing Spacelift budget hooks"
-    TemporaryResource = "true"
-  }
-}
+#resource "aws_instance" "budget_test" {
+#  ami           = data.aws_ami.amazon_linux.id
+#  instance_type = "t2.micro"
+#  subnet_id     = tolist(data.aws_subnets.default.ids)[0]
+#  
+#  tags = {
+#    Name = "BudgetTestInstance"
+#    Purpose = "Testing Spacelift budget hooks"
+#    TemporaryResource = "true"
+#  }
+#}

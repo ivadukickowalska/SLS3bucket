0ae9d763b51da0d79be4c671b79d97ff31bd7c93
Iva Dukic Kowalska <138592614+ivadukickowalska@users.noreply.github.com>
2025-08-10 21:43:17 +0200

    Update ec2-t2-micro-config.tf
    updated using claude recommendations

--- ec2-t2-micro-config.tf
+++ ec2-t2-micro-config.tf
@@ -1,6 +1,26 @@
+# Find latest Amazon Linux 2 AMI
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# Create EC2 instance using the found AMI
 resource "aws_instance" "budget_test" {
-  ami           = "ami-0230bd60aa48260c6"  # Amazon Linux 2023 AMI in us-east-1
+  ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
+  
+  # This is required because EC2 instances need a subnet
+  subnet_id     = aws_vpc.main.subnet_ids[0]  # Use an existing subnet from your VPC
   
   tags = {
     Name = "BudgetTestInstance"

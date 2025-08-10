2371f4623be22305ef9995173e2ac941a16be859
Iva Dukic Kowalska <138592614+ivadukickowalska@users.noreply.github.com>
2025-08-10 21:45:54 +0200

    Update ec2-t2-micro-config.tf

--- ec2-t2-micro-config.tf
+++ ec2-t2-micro-config.tf
@@ -1,13 +1,29 @@
-# Get default VPC (if you don't have one already)
+# Get default VPC
 data "aws_vpc" "default" {
   default = true
 }
 
-# Get default subnet in the first availability zone
+# Get default subnets
 data "aws_subnets" "default" {
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
+  }
+}
+
+# Find latest Amazon Linux 2 AMI
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
 
